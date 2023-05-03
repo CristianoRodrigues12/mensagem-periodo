a28d95bd1fe48c5d20c7954206ae0cd7d7f15da4
CristianoRodrigues12 <kriks.red@gmail.com>
2023-05-03 18:58:57 -0300

    Mensagem periodo corrente!

---
+++ Mensagem/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Exercício mensagem período");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+    }
+}
